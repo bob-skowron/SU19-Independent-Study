7e5c2323fd8d2ad17d3b5406017117b17f92bd26
Bob Skowron <bob.skowron@gmail.com>
2019-06-22 21:19:33 -0500

    Updated source data logic to include compustat indices
    Compustat has more indices that are both bigger and smalller. This should give us additional insights into the feasibility in large indices. Also, included GICs classifications as well so that we could potentially look at alternatives (largest x by mkt cap, top x in each sector, etc.)

--- data/source-data.py
+++ data/source-data.py
@@ -4,18 +4,52 @@ import pandas as pd
 import os
 
 
-
+os.getcwd()
 # list tables in the crsp dataset
-db.list_tables(library="crsp")
+db.list_tables(library="compd")
+
+# get values from a single table
+db.get_table(library="compd",table="idxcst_his",obs=10)
+print(db.get_table(library="compd",table="co_hgic",obs=10).to_string())
+print(db.get_table(library="compd",table="r_giccd",obs=10).to_string())
 
 # select top 10 records from SP500 daily table to test connection
 db.raw_sql("select caldt, spindx, sprtrn, totval from crsp.dsp500 limit 10")
 db.raw_sql("select caldt, spindx, sprtrn, totval from crsp.dsp500 where caldt >= '1989-01-01' limit 10")
 
-db.raw_sql("select a.*, b.ret, b.date from crsp.dsp500list as a join crsp.dsf as b on a.permno=b.permno and b.date >= a.start and b.date<= a.ending where b.date = '2018-12-31' order by date LIMIT 500;")
+db.raw_sql("select *  from compd.secd where gvkey='001487' and datadate='2018-01-31'")
+db.raw_sql("select *  from compd.idxcst_his h where gvkey='001487' and h.from <='2018-01-31' and case when h.thru is null then '9999-12-31' else h.thru end >= '2018-01-31'")
 
-db.get_table(library="crsp",table="SICCD",obs=10)
+db.raw_sql(f"""select s.datadate, n.gvkeyx, s.gvkey, n.conm, s.tic, s.prccd as close, s.cshoc*s.prccd as mktcap, 
+        rgsector.gicdesc as gsector, rggroup.gicdesc as ggroup, rgind.gicdesc as gind, rgsubind.gicdesc as gsubind
+    from compd.names_ix n 
+    join compd.idxcst_his c 
+    on n.gvkeyx = c.gvkeyx
+    join compd.secd s
+    on c.gvkey = s.gvkey
+    and s.iid = c.iid
+    and s.datadate >= c.from
+    and s.datadate <= case when c.thru is null then '9999-12-31' else c.thru end
 
+    join compd.co_hgic g
+    on s.gvkey = g.gvkey
+    and s.datadate >= g.indfrom
+    and s.datadate <= case when g.indthru is null then '9999-12-31' else g.indthru end
+
+    join compd.r_giccd rgsector
+    on g.gsector = rgsector.giccd and rgsector.gictype = 'GSECTOR'
+
+    join compd.r_giccd rggroup
+    on g.ggroup = rggroup.giccd and rggroup.gictype = 'GGROUP'
+
+    join compd.r_giccd rgind
+    on g.gind = rgind.giccd and rgind.gictype = 'GIND'
+
+    join compd.r_giccd rgsubind
+    on g.gsubind = rgsubind.giccd and rgsubind.gictype = 'GSUBIND'
+
+    where n.gvkeyx='000003' 
+    and s.datadate = '2018-01-31' LIMIT 550""")
 
 
 def wrds_connect():
@@ -40,6 +74,48 @@ def get_sp500_const_return_data(db, startdate, enddate):
     order by s.date, sn.ticker""")
     return data 
 
+def get_idx_return_data(db, startdate, enddate, idx_list):
+    data = db.raw_sql(f"""select d.datadate, n.gvkeyx, n.conm, d.prccd as close, d.prccddiv as totalreturn 
+    from compd.names_ix n join compd.idx_daily d 
+    on n.gvkeyx = d.gvkeyx 
+    where n.gvkeyx in ('{"','".join(idx_list)}') 
+    and datadate >= '{startdate}' and datadate <= '{enddate}'""")
+    return data
+
+def get_idx_const_return_data(db, startdate, enddate, idx_list):
+    db.raw_sql(f"""select s.datadate, n.gvkeyx, s.gvkey, n.conm, s.tic, s.prccd as close, s.cshoc*s.prccd as mktcap, 
+            rgsector.gicdesc as gsector, rggroup.gicdesc as ggroup, rgind.gicdesc as gind, rgsubind.gicdesc as gsubind
+        from compd.names_ix n 
+        join compd.idxcst_his c 
+        on n.gvkeyx = c.gvkeyx
+        join compd.secd s
+        on c.gvkey = s.gvkey
+        and s.iid = c.iid
+        and s.datadate >= c.from
+        and s.datadate <= case when c.thru is null then '9999-12-31' else c.thru end
+
+        join compd.co_hgic g
+        on s.gvkey = g.gvkey
+        and s.datadate >= g.indfrom
+        and s.datadate <= case when g.indthru is null then '9999-12-31' else g.indthru end
+
+        join compd.r_giccd rgsector
+        on g.gsector = rgsector.giccd and rgsector.gictype = 'GSECTOR'
+
+        join compd.r_giccd rggroup
+        on g.ggroup = rggroup.giccd and rggroup.gictype = 'GGROUP'
+
+        join compd.r_giccd rgind
+        on g.gind = rgind.giccd and rgind.gictype = 'GIND'
+
+        join compd.r_giccd rgsubind
+        on g.gsubind = rgsubind.giccd and rgsubind.gictype = 'GSUBIND'
+
+        where n.gvkeyx in ('{"','".join(idx_list)}') 
+        and s.datadate >= '{startdate}' and s.datadate <= '{enddate}'""")
+    return data
+
+
 
 if __name__ == "__main__":
     
@@ -54,6 +130,11 @@ if __name__ == "__main__":
     # get const data and write to file
     const_data = get_sp500_const_return_data(db, startdate, enddate)
 
+    # get more index data from compustat
+    # S&P500, Russell 2000, Russell 3000, Wilshire 5000, S&P 1000 (midcap), S&P600 (smallcap), S&P 100 (large cap)
+    idx_list = ["000003", "156765", "156931", "182779", "146884", "030824", "000664"]
+    more_idx_data = get_idx_return_data(db, startdate, enddate, idx_list)
+    more_idx_const = get_idx_const_return_data(db, startdate, enddate, idx_list)
 
     # close the connection
     db.close()
